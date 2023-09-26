3440f57f0c86ac1e58b1ccf6f6a8b4c1466ae63f
Haochen Sam Zhu <73946565+JinkelaP@users.noreply.github.com>
2023-09-26 18:53:00 +1300

    Update CinemaController.py

--- CinemaController.py
+++ CinemaController.py
@@ -7,6 +7,7 @@ from Ticket import Ticket
 from User import Customer, Admin, Staff
 
 from decimal import Decimal
+from datetime import date, datetime
 
 class Cinema:
     def __init__(self) -> None:
@@ -17,8 +18,41 @@ class Cinema:
         self.allMovie = []
         self.allScreening = []
 
-    def browseMovie(self) -> list:
-        return self.allMovie
+    def browseAllMovie(self) -> list:
+        if self.allMovie == []:
+            return self.allMovie
+        else:
+            return self.allMovie.sort(key = lambda movie: movie.releaseDate)
+    
+    # Handle all search except date
+    def searchMovieByStr(self, searching: str) -> list:
+        result = []
+        for i in self.allMovie:
+            if searching in i.movieName or searching in i.lang or searching in i.genre:
+                result.append(i)
+        return result
+    
+    # Handle date search
+    def searchMovieByDateAfter(self, searching: date) -> list:
+        result = []
+        for i in self.allMovie:
+            if i.releaseDate > searching :
+                result.append(i)
+        if result == []:
+            return result
+        else:
+            return result.sort(key = lambda movie: movie.releaseDate)
+        
+    def movieDetail(self, movie: Movie) -> str:
+        if movie in self.allMovie:
+            return f'Title: {movie.movieName}\nLanguage: {movie.lang}\nGenre: {movie.genre}\nDate of Release: {movie.releaseDate}\nDuration: {movie.duration}'
+        else:
+            return f'Movie not found!'
+    
+    def movieSchedule(self, movie: Movie) -> list:
+        result = []
+
+
     
 
 
