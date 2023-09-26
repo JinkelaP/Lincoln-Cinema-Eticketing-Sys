6c596e4362a094bafa1db9ccbe32a63aa7d1ad99
Haochen Sam Zhu <73946565+JinkelaP@users.noreply.github.com>
2023-09-26 18:22:44 +1300

    functions

--- CinemaController.py
+++ CinemaController.py
@@ -1,13 +1,26 @@
-from Movie import Movie
+from Coupon import Coupon
 from Hall import Hall
+from Movie import Movie
+from Screening import Screening
+from Seat import Seat
+from Ticket import Ticket
+from User import Customer, Admin, Staff
 
 from decimal import Decimal
 
-class Clinic:
-    def __init__(self):
-        self.allDoctors = []
-        self.allPatients = []
-        self.allConsultations = []
+class Cinema:
+    def __init__(self) -> None:
+        self.allCustomer = []
+        self.allStaff = []
+        self.allAdmin = []
+        self.allHall = [Hall(1, 120), Hall(2, 90), Hall(3, 110), Hall(4, 80)]
+        self.allMovie = []
+        self.allScreening = []
+
+    def browseMovie(self) -> list:
+        return self.allMovie
+    
+
 
 
 
--- User.py
+++ User.py
@@ -3,9 +3,11 @@ from abc import ABC, abstractmethod
 class User(ABC):
     nextID = 10
 
-    def __init__(self, name: str) -> None:
+    def __init__(self, name: str, psw: str) -> None:
         self._username = name
         self._userID = User.nextID
+        self._userPassword = psw
+        self._userMsg = []
         User.nextID += 1
 
     def __str__(self) -> str:
@@ -20,14 +22,45 @@ class User(ABC):
     def userID(self) -> int:
         return self._userID
     
+    @property
+    def userPassword(self) -> int:
+        return self._userPassword
+    
+    @property
+    def userMsg(self) -> int:
+        return self._userMsg
+    
     @username.setter
     def usernameSet(self, name: str) -> None:
         self._username = name
 
-class Customer(User):
+    @userPassword.setter
+    def userPasswordSet(self, psw: str) -> None:
+        self._userPassword = psw
 
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
+    @userMsg.setter
+    def userMsgSet(self, msg: str) -> None:
+        self._userMsg.append(msg)
+
+class Customer(User):
+    def __init__(self, name: str, psw: str) -> None:
+        super().__init__(name, psw)
         self.__ticket = []
         self.__coupon = []
 
+    @property
+    def ticket(self) -> list:
+        return self.__ticket
+    
+    @property
+    def coupon(self) -> list:
+        return self.__coupon
+                                                                 
+
+class Staff(User):
+    def __init__(self, name: str, psw: str) -> None:
+        super().__init__(name, psw)
+
+class Admin(User):
+    def __init__(self, name: str, psw: str) -> None:
+        super().__init__(name, psw)
